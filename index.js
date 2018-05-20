b95d6a9813e36cd060f6d8b10b7500555f2828e5
Marcia Elyseu <marcia.pelyseu@gmail.com>
2018-05-20 22:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays() {
 happyHolidays();
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 happyHolidaysTo("Tania");
